24ead32fa2d067d8bdc28bb0087292c59aaa78d3
VallKrupp <vkrupitsa@gmail.com>
2021-10-07 15:11:43 +0300

    Adding fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 import "bootstrap"
-import '../js/bootstrap_js_files.js'  
+import '../js/bootstrap_js_files.js'
+import "@fortawesome/fontawesome-free/css/all"  
 import "stylesheets/application"
 Rails.start()
 Turbolinks.start()
