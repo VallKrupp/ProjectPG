de4f8934cf94c58770332b2dab67fb69cc1a75b0
VallKrupp <vkrupitsa@gmail.com>
2021-10-08 12:53:54 +0300

    adding Dropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 import "bootstrap"
-import '../js/bootstrap_js_files.js'
 import "@fortawesome/fontawesome-free/css/all"  
 import "stylesheets/application"
 Rails.start()
