6ac37726865b7d090209812571216a5052e464d6
VallKrupp <vkrupitsa@gmail.com>
2021-10-09 13:53:12 +0300

    Adding user page for future administration system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks:'users/omniauth_callbacks' }
   root 'static_pages#landing_page'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :users, only: %i[index]
 end
