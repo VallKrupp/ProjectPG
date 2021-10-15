aa2e12b81e21554c65132fcc10b067dd6cdab88d
VallKrupp <vkrupitsa@gmail.com>
2021-10-15 19:21:13 +0300

    last one at a friday

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,4 +4,19 @@ class AnswersController < ApplicationController
     @answer = Answer.new
   end
 
+  def create
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      flash[:success] = "Correct answer is added"
+      redirect_to root_url
+    else
+      render :new, status: :unprocessable_entity 
+    end
+  end
 end
+
+private
+    # Only allow a list of trusted parameters through.
+    def answer_params
+      params.require(:answer).permit(:body)
+    end
