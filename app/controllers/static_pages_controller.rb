a35ee7fd139f84e9f6358ab868126158fcd450a6
VallKrupp <vkrupitsa@gmail.com>
2021-11-18 17:17:27 +0300

    Updating donut graph with Chart.js

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[landing_page]
-
   def landing_page
-
+    @data_keys = [
+      'Google',
+      'GitHub',
+      'Self',
+    ]
+    @data_values = User.user_providers.values
   end
+
 end
